3ec9ba2d3e49d01724b8482babedd6b67c8d01e6
Sergey Eroshevich <eroshik.test@gmail.com>
2022-07-01 15:53:32 +0300

    add cart

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,7 @@ from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
+    print()
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
