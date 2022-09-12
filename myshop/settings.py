df1675c2864470a3d39a64bd33042ff20c1b79c1
SergeyEroshevich <101111220+SergeyEroshevich@users.noreply.github.com>
2022-09-12 11:12:31 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -161,7 +161,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.yandex.by'
-EMAIL_HOST_USER = 'micromagic.by@yandex.by'
-EMAIL_HOST_PASSWORD = 'R1hkdf58223s'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
